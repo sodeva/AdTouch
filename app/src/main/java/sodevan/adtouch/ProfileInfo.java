a77a90ed5701ad9feb2da2f7e0b43ecbdd1db2ff
ravipiyush <piyush.gupta111019@gmail.com>
2016-10-09 12:35:25 +0530

    Facebook Api

--- app/src/main/java/sodevan/adtouch/ProfileInfo.java
+++ app/src/main/java/sodevan/adtouch/ProfileInfo.java
@@ -3,6 +3,7 @@ package sodevan.adtouch;
 import android.content.SharedPreferences;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Typeface;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Build;
@@ -16,6 +17,7 @@ import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -41,14 +43,16 @@ import java.net.URL;
 
 public class ProfileInfo extends AppCompatActivity {
 
-    String bday ;
+    TextView bday, agerange , eduaction , email , hometown , location , relation , religion  ;
+
     TextView tvbd ;
     URI juri  ;
     URL url ;
+    Typeface cav , robo;
     Bitmap image  ;
 
-    CircularImageView circularImageView ;
 
+    public CircularImageView circularImageView ;
 
 
     FirebaseAuth auth ;
@@ -57,15 +61,50 @@ public class ProfileInfo extends AppCompatActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_info);
+
+
+        cav = Typeface.createFromAsset(getAssets() ,"caviar.ttf") ;
+        robo = Typeface.createFromAsset(getAssets() ,"Roboto-Light.ttf");
+
+        bday =(TextView)findViewById(R.id.birhday) ;
+        eduaction =(TextView)findViewById(R.id.education) ;
+        agerange =(TextView)findViewById(R.id.Agerange) ;
+        email = (TextView)findViewById(R.id.email);
+        hometown=(TextView)findViewById(R.id.hometown) ;
+        location=(TextView)findViewById(R.id.location);
+        relation=(TextView)findViewById(R.id.rstatus) ;
+        religion=(TextView)findViewById(R.id.religion) ;
+
+        bday.setTypeface(cav);
+        eduaction.setTypeface(cav);
+        agerange.setTypeface(cav);
+        email.setTypeface(cav);
+        hometown.setTypeface(cav);
+        location.setTypeface(cav);
+        relation.setTypeface(cav);
+        religion.setTypeface(cav);
+
+
+        tvbd = (TextView)findViewById(R.id.Username) ;
+        tvbd.setTypeface(robo);
+
+
+
 
 
         auth = FirebaseAuth.getInstance() ;
 
+
+
         FirebaseUser user = auth.getCurrentUser() ;
+        String name = user.getDisplayName() ;
+
+        tvbd.setText(name);
+
 
 
         circularImageView = (CircularImageView)findViewById(R.id.imageView) ;
-
 
 
 
@@ -81,16 +120,13 @@ public class ProfileInfo extends AppCompatActivity {
 
         try {
             url  = juri.toURL() ;
-
         } catch (MalformedURLException e) {
             e.printStackTrace();
         }
 
         Log.e("Url : ", url.toString()) ;
-        new display().execute() ;
 
-
-
+        new display().execute(url) ;
     }
 
 
@@ -161,29 +197,35 @@ public class ProfileInfo extends AppCompatActivity {
 
 
     // This AsyncTask is To get Image from Image url
-    private  class display extends AsyncTask<Void ,Void, Void> {
+    private  class display extends AsyncTask<URL ,Void, Bitmap> {
 
         @Override
-        protected Void doInBackground(Void... voids) {
+        protected Bitmap doInBackground(URL... param) {
 
+            Bitmap image1 = null;
             try {
-                image = BitmapFactory.decodeStream(url.openConnection().getInputStream());
-            }
-            catch (IOException e) {
+                url = param[0];
+                Log.e("ThredUrl", url.toString());
+                image1 = BitmapFactory.decodeStream(url.openConnection().getInputStream());
+            } catch (IOException e) {
                 e.printStackTrace();
             }
 
-            return null;
+            return image1;
         }
 
 
         @Override
-        protected void onPostExecute(Void aVoid) {
+        protected void onPostExecute(Bitmap aVoid) {
 
             super.onPostExecute(aVoid);
-            circularImageView.setImageBitmap(Bitmap.createScaledBitmap(image, 350, 350, false));
-
-        }
+            //circularImageView.setImageBitmap(Bitmap.createScaledBitmap(aVoid, 350, 350, false));
+            if(circularImageView != null) {
+                circularImageView.setImageBitmap(aVoid);
+            }else{
+                Log.e("BitmapNull", "Bitmap Null");
+            }
+            }
     }
 
 
